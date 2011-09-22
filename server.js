e997428f2d7b9dbc57fb18b36858a466f5148215
Evan Prodromou <evan@status.net>
2011-09-22 17:43:41 -0400

    First working version
    This seems to be a working version; I haven't checked it out very carefully.
    
    I'm going to work on training it with a real corpus of data to get some sense of whether it's working.

--- server.js
+++ server.js
@@ -57,6 +57,19 @@ function tokenize(obj)
     return tokens;
 }
 
+function uniq(arr)
+{
+    var newArr = [];
+
+    for (i in arr) {
+	if (newArr.indexOf(arr[i]) == -1) {
+	    newArr.push(arr[i]);
+	}
+    }
+
+    return newArr;
+}
+
 function updateSpamCount(r, token, spam_total, not_spam_total)
 {
     r.incr('spam:'+token, function(err, spam_count) {
@@ -64,6 +77,23 @@ function updateSpamCount(r, token, spam_total, not_spam_total)
 	    var g = 2 * not_spam_count;
 	    var b = spam_count;
 	    if (g + b > 5) { // This will make id=... values kinda useless
+		var p = Math.max(0.01,
+				 Math.min(0.99,
+					  Math.min(1, b/spam_total)/
+					  (Math.min(1, g/not_spam_total) + Math.min(1, b/spam_total))));
+		r.set('prob:'+token, p);
+	    }
+	});
+    });
+}
+
+function updateNotSpamCount(r, token, spam_total, not_spam_total)
+{
+    r.incr('not-spam:'+token, function(err, not_spam_count) {
+	r.get('spam:'+token, function(err, spam_count) {
+	    var g = 2 * not_spam_count;
+	    var b = spam_count;
+	    if (g + b > 5) {
 		var p = Math.max(0.01,
 				 Math.min(0.99,
 					  Math.min(1, b/spam_total)/
@@ -90,46 +120,75 @@ function updateSpamCounts(tokens, onSuccess)
     });
 }
 
-function updateSpamProbabilities(counts, onSuccess)
-{
-    var probabilities = [];
-    // TODO: update probabilities
-    onSuccess(probabilities);
-}
-
 function updateNotSpamCounts(tokens, onSuccess)
 {
-    var counts = [];
-    // TODO: update counts
-    onSuccess(counts);
-}
+    var r = redis.createClient();
 
-function updateNotSpamProbabilities(counts, onSuccess)
-{
-    var probabilities = [];
-    // TODO: update probabilities
-    onSuccess(probabilities);
+    r.stream.on('connect', function() {
+	r.incr('notspamtotal', function(err, not_spam_total) {
+	    r.get('spamtotal', function(err, spam_total) {
+		for (i in tokens) { // Not sure I love this
+		    updateNotSpamCount(r, tokens[i], spam_total, not_spam_total);
+		}
+		onSuccess();
+	    });
+	});
+    });
 }
 
 function getProbabilities(tokens, onSuccess)
 {
-    var probabilities = [];
-    // TODO: fetch probabilities
-    onSuccess(probabilities);
+    var probkeys = [];
+    for (i in tokens) {
+	probkeys.push('prob:'+tokens[i]);
+    }
+    
+    var r = redis.createClient();
+
+    r.mget(probkeys, function(err, probs) {
+	probabilities = [];
+	for (i in tokens) {
+	    // There's probably a nicer data structure for this
+	    if (probs[i] == null) {
+		probabilities[i] = [tokens[i], 0.4];
+	    } else {
+		probabilities[i] = [tokens[i], probs[i]];
+	    }
+	}
+	onSuccess(probabilities);
+    });
 }
 
 function bestProbabilities(probs)
 {
-    var bestProbs = [];
-    // TODO: determine the best probabilities
-    return bestProbs;
+    probs.sort(function (a, b) {
+	var adist = Math.abs(a[1] - 0.5);
+	var bdist = Math.abs(b[1] - 0.5);
+	
+	if (adist > bdist) {
+	    return -1;
+	} else if (bdist > adist) {
+	    return 1;
+	} else {
+	    return 0;
+	}
+    });
+
+    // Get the best 15
+    return probs.slice(0, Math.min(probs.length, 15));
 }
 
 function combineProbabilities(probs)
 {
-    var result = 0.5;
-    // TODO: determine the best probabilities
-    return result;
+    var prod = probs.reduce(function(coll, cur, index, array) {
+	return coll * cur[1];
+    }, 1.0);
+
+    var invprod = probs.reduce(function(coll, cur, index, array) {
+	return coll * (1 - cur[1]);
+    }, 1.0);
+
+    return (prod)/(prod + invprod); // really?
 }
 
 function thisIsSpam(req, res, next) {
@@ -149,10 +208,13 @@ function thisIsNotSpam(req, res, next) {
 }
 
 function isThisSpam(req, res, next) {
-    var tokens = tokenize(req.body);
+    var tokens = uniq(tokenize(req.body));
     getProbabilities(tokens, function(probs) {
 	var bestprobs = bestProbabilities(probs);
-	var decision = combineProbabilities(bestprobs);
+	var prob = combineProbabilities(bestprobs);
+	var decision = { probability: prob,
+			 isSpam: ((prob > 0.90) ? true : false),
+			 bestKeys: bestprobs };
 	res.writeHead(200, {'Content-Type': 'application/json'});
 	res.end(JSON.stringify(decision));
     });
