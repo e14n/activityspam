f6ac24a36c7644a6399d0ae3787e1d713d6dab1d
Evan Prodromou <evan@status.net>
2012-02-09 13:37:15 -0500

    a utility to post a single activity

---
+++ scripts/postactivity.js
@@ -0,0 +1,44 @@
+// postactivity.js
+//
+// Post an activity streams activity somewhere
+//
+// Copyright 2012, StatusNet Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var fs = require('fs'),
+    common = require('./common'),
+    postActivity = common.postActivity;
+
+if (process.argv.length != 5) {
+    process.stderr.write("USAGE: node postactivity.js username:password filename.json URL\n");
+    process.exit(1);
+}
+
+var auth = process.argv[2];
+var fileName = process.argv[3];
+var serverUrl = process.argv[4];
+
+fs.readFile(fileName, function (err, data) {
+    var i, activity;
+
+    if (err) {
+	console.log("Error reading file: " + err);
+	process.exit(1);
+    }
+
+    activity = JSON.parse(data);
+
+    postActivity(serverUrl, auth, activity);
+});
+
